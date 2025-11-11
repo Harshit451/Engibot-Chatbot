0569bdd123afb34ad1a366116543c32906545367
Harshit451 <shardaharshit42@gmail.com>
2025-11-11 22:32:36 +0530

    updated url

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ async function callGeminiAPI(historyMessages = []) {
 
     console.log("📤 Sending formatted messages to backend:", formattedMessages);
 
-    const res = await fetch('https://engibot-chatbot.onrender.com/api/chat', {
+    const res = await fetch('https://engibot-backend.onrender.com/api/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ messages: formattedMessages })
